8095a2addd520f3af2bea1cacd9ba3a3b70f8320
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-02-22 21:22:47 -0800

    Avoiding prop drilling with children pattern

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,36 @@
 import { useEffect, useState } from "react";
+
 import Background from "./Background";
 import Container from "./Container";
 import Footer from "./Footer";
-import Header from "./Header";
+import Header, { HeaderTop } from "./Header";
+import BookmarksButton from "./BookmarksButton";
+import Logo from "./Logo";
+import SearchForm from "./SearchForm";
+import JobItemContent from "./JobItemContent";
+import Sidebar, { SidebarTop } from "./Sidebar";
+import JobList from "./JobList";
+import PaginationControls from "./PaginationControls";
+import ResultsCount from "./ResultsCount";
+import SortingControls from "./SortingControls";
 
 function App() {
   const [searchText, setSearchText] = useState("");
   const [jobItems, setJobItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (!searchText) return;
 
     const fetchJobs = async () => {
+      setIsLoading(true);
       try {
         const response = await fetch(
           `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
         );
 
         const data = await response.json();
+        setIsLoading(false);
         setJobItems(data.jobItems);
       } catch (error) {
         console.log(error);
@@ -31,9 +44,28 @@ function App() {
     <>
       <Background />
 
-      <Header searchText={searchText} setSearchText={setSearchText} />
+      <Header>
+        <HeaderTop>
+          <Logo />
+          <BookmarksButton />
+        </HeaderTop>
 
-      <Container jobItems={jobItems} />
+        <SearchForm setSearchText={setSearchText} searchText={searchText} />
+      </Header>
+
+      <Container>
+        <Sidebar>
+          <SidebarTop>
+            <ResultsCount />
+            <SortingControls />
+          </SidebarTop>
+
+          <JobList jobItems={jobItems} isLoading={isLoading} />
+
+          <PaginationControls />
+        </Sidebar>
+        <JobItemContent />
+      </Container>
 
       <Footer />
     </>
