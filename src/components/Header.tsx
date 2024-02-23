a505da33af9236360d5b66ece9b3fc61d308f971
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-02-22 21:04:39 -0800

    Data now fetchable and displayed on sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import BookmarksButton from "./BookmarksButton";
 import Logo from "./Logo";
 import SearchForm from "./SearchForm";
 
-export default function Header() {
+export default function Header({ searchText, setSearchText }) {
   return (
     <header className="header">
       <div className="header__top">
@@ -10,7 +10,7 @@ export default function Header() {
         <BookmarksButton />
       </div>
 
-      <SearchForm />
+      <SearchForm setSearchText={setSearchText} searchText={searchText} />
     </header>
   );
 }
