d7f771a9f183a8678912000089ba3d35c0822633
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-02-24 22:49:09 -0800

    Add error handling when fetching data

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -11,6 +11,12 @@ type JobItemApiResponse = {
 
 const fetchJobItem = async (id: number | null): Promise<JobItemApiResponse> => {
   const response = await fetch(`${BASE_API_URL}/${id}`);
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.description);
+  }
+
   const data = await response.json();
   return data;
 };
