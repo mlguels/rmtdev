6460627521fdc22525571e6447aff621a001f650
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-03-03 16:25:38 -0800

    Add: close popover on click with className

--- src/components/BookmarksButton.tsx
+++ src/components/BookmarksButton.tsx
@@ -1,10 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { TriangleDownIcon } from "@radix-ui/react-icons";
 import BookmarksPopover from "./BookmarksPopover";
 
 export default function BookmarksButton() {
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (
+        e.target instanceof HTMLElement &&
+        !e.target.closest(".bookmarks-btn") &&
+        !e.target.closest(".bookmarks-popover")
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, []);
   return (
     <section>
       <button
